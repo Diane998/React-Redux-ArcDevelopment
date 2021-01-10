f548a527bf40ed843cbb353a9dcfa650b3b778ef
Diane998 <gutierrez.almiradyan@gmail.com>
2021-01-10 16:58:28 +0800

    Refactored services page

--- src/components/pages/Services.js
+++ src/components/pages/Services.js
@@ -1,5 +1,213 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { useTheme, makeStyles } from '@material-ui/core/styles';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
 
-const Services = () => <div>Services</div>;
+import { Grid, Button, Typography } from '@material-ui/core';
+import ButtonArrow from '../ui/ButtonArrow';
+
+import OneColumnSection from '../section-templates/OneColumnSection';
+
+import customSoftwareIcon from '../../assets/Custom Software Icon.svg';
+import mobileAppsIcon from '../../assets/mobileIcon.svg';
+import websitesIcon from '../../assets/websiteIcon.svg';
+
+const useStyles = makeStyles(theme => ({
+  mainContainer: { marginBottom: '8em', width: '100%' },
+  container: {
+    marginTop: '5em',
+    [theme.breakpoints.down('sm')]: {
+      padding: 25
+    }
+  },
+  subtitle: {
+    marginBottom: '1em'
+  },
+  specialText: {
+    fontFamily: 'Pacifico',
+    color: theme.palette.common.orange
+  },
+  button: {
+    ...theme.typography.learnButton,
+    fontSize: '0.7rem',
+    height: 35,
+    padding: 5,
+    margin: '0.5em 0em 1em 0em'
+  },
+  icon: {
+    marginLeft: '2em',
+    [theme.breakpoints.down('xs')]: {
+      marginLeft: 0
+    }
+  }
+}));
+
+const services = [
+  {
+    title: 'Custom Software Development',
+    route: '/services',
+    nestedRoute: '/customsoftware',
+    subtitles: ['Save Energy. Save Time. Save Money.'],
+    specialText: [
+      {
+        text: 'Complete digital solutions, from investigation to ',
+        span: 'celebration.'
+      }
+    ],
+    image: customSoftwareIcon,
+    alt: 'custom software icon'
+  },
+  {
+    title: 'IOS/Android App Development',
+    route: '/services',
+    nestedRoute: '/mobileapps',
+    subtitles: [
+      ' Extend Functionality. Extend Access. Increase Engagement.',
+      'Integrate your web experience or create a standalone app with either mobile platform.'
+    ],
+    image: mobileAppsIcon,
+    alt: 'mobile app icon'
+  },
+  {
+    title: 'Website Development',
+    route: '/services',
+    nestedRoute: '/websites',
+    subtitles: [
+      'Reach More. Discover More. Sell More.',
+      'Optimized for Search Engines, built for speed.'
+    ],
+    image: websitesIcon,
+    alt: 'website icon'
+  }
+];
+
+const renderSubtitles = arr =>
+  arr.map((subtitle, i) => (
+    <Typography key={i} variant="subtitle1">
+      {subtitle}
+    </Typography>
+  ));
+
+const renderSpecialText = (arr, classes) =>
+  arr.map(({ text, span }, i) => (
+    <Typography key={i} variant="subtitle1">
+      {text}
+      <span className={classes}>{span}</span>
+    </Typography>
+  ));
+
+const Services = props => {
+  const classes = useStyles();
+  const theme = useTheme();
+  const matchesSM = useMediaQuery(theme.breakpoints.down('sm'));
+
+  const { setTabIndex, match } = props;
+
+  return (
+    <Grid container direction="column" className={classes.mainContainer}>
+      <Grid
+        style={{
+          margin: matchesSM ? '1em 0 0 0' : '1em 0 0 5em',
+          textAlign: matchesSM ? 'center' : ''
+        }}
+        item
+      >
+        <Typography variant="h2" gutterBottom>
+          Services
+        </Typography>
+      </Grid>
+      <>
+        {services.map((section, i) =>
+          i % 2 === 0 ? (
+            <OneColumnSection
+              key={i}
+              container
+              direction="row"
+              justify={matchesSM ? 'center' : undefined}
+              className={classes.container}
+            >
+              <Grid
+                item
+                style={{
+                  marginLeft: matchesSM ? 0 : '5em',
+                  textAlign: matchesSM ? 'center' : ''
+                }}
+              >
+                <Typography variant="h4">{section.title}</Typography>
+                {renderSubtitles(section.subtitles)}
+                {section.specialText
+                  ? renderSpecialText(section.specialText, classes.specialText)
+                  : null}
+                <Button
+                  component={Link}
+                  to={`${section.route}${section.nestedRoute}`}
+                  className={classes.button}
+                  variant="outlined"
+                  onClick={() => setTabIndex(1)}
+                >
+                  <span style={{ marginRight: 5 }}>Learn More</span>
+                  <ButtonArrow
+                    width={10}
+                    height={10}
+                    fill={theme.palette.common.blue}
+                  />
+                </Button>
+              </Grid>
+              <Grid item>
+                <img
+                  className={classes.icon}
+                  src={section.image}
+                  alt={section.alt}
+                />
+              </Grid>
+            </OneColumnSection>
+          ) : (
+            <OneColumnSection
+              key={i}
+              container
+              direction="row"
+              justify={matchesSM ? 'center' : 'flex-end'}
+              className={classes.container}
+            >
+              <Grid
+                item
+                style={{
+                  textAlign: matchesSM ? 'center' : undefined
+                }}
+              >
+                <Typography variant="h4">{section.title}</Typography>
+                {renderSubtitles(section.subtitles)}
+                {section.specialText
+                  ? renderSpecialText(section.specialText, classes.specialText)
+                  : null}
+                <Button
+                  component={Link}
+                  to={`${section.route}${section.nestedRoute}`}
+                  className={classes.button}
+                  variant="outlined"
+                  onClick={() => setTabIndex(1)}
+                >
+                  <span style={{ marginRight: 5 }}>Learn More</span>
+                  <ButtonArrow
+                    width={10}
+                    height={10}
+                    fill={theme.palette.common.blue}
+                  />
+                </Button>
+              </Grid>
+              <Grid item style={{ marginRight: matchesSM ? 0 : '5em' }}>
+                <img
+                  className={classes.icon}
+                  src={section.image}
+                  alt={section.alt}
+                />
+              </Grid>
+            </OneColumnSection>
+          )
+        )}
+      </>
+    </Grid>
+  );
+};
 
 export default Services;
